0328ef0cc31b7a4b04d97634b7e2de56f62184b8
urmil404 <urmil8989@gmail.com>
2022-06-03 06:57:57 +0530

    everything in 1 file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const App = () =>
+{
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Hello World!</h1>
+      <button className="button">Hello ji</button>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-function Appy()
-{
-    return (
-        <h1>Hello World!</h1>
-    );
-}
 
-ReactDOM.render(<Appy/>, document.getElementById("root"));
 
+
+ReactDOM.render(<App />, document.getElementById("root"));
